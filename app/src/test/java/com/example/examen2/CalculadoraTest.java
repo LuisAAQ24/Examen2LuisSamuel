777e2f6dac7368dd46f61722f0c381d6d47cff50
Samuel Morales Cascante <sarimoca@gmail.com>
2024-11-10 19:23:37 -0600

    Pruebas unitarias faltantes de Samuel Morales

--- app/src/test/java/com/example/examen2/CalculadoraTest.java
+++ app/src/test/java/com/example/examen2/CalculadoraTest.java
@@ -12,7 +12,10 @@ public class CalculadoraTest {
         assertEquals(5, Calculadora.convertirNumero("101", 0)); // Caso base: 101 en binario es 5 en decimal
     }
 
-
+    @Test
+    public void testConvertirNumeroHexadecimal() {
+        assertEquals(255, Calculadora.convertirNumero("FF", 3)); // Caso base: FF en hexadecimal es 255 en decimal
+    }
 
     // Test para el método realizarOperacion
     @Test
@@ -20,7 +23,10 @@ public class CalculadoraTest {
         assertEquals(15, Calculadora.realizarOperacion(10, 5, "+")); // Caso base: 10 + 5 = 15
     }
 
-
+    @Test
+    public void testRealizarOperacionDivision() {
+        assertEquals(2, Calculadora.realizarOperacion(10, 5, "/")); // Caso base: 10 / 5 = 2
+    }
 
     // Test para el método convertirADestino
     @Test
@@ -28,7 +34,10 @@ public class CalculadoraTest {
         assertEquals("1010", Calculadora.convertirADestino(10, 0)); // Caso base: 10 en decimal es 1010 en binario
     }
 
-
+    @Test
+    public void testConvertirADestinoHexadecimal() {
+        assertEquals("a", Calculadora.convertirADestino(10, 3)); // Caso base: 10 en decimal es a en hexadecimal
+    }
 
 }
 
